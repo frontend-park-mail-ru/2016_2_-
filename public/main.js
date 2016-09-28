80b4715bf0bb8fbfdf150c0fdbdc4950c22e26e5
valera <noking007@mail.ru>
2016-09-29 02:51:38 +0300

    прошелся линтером, все исправил, осталось то что нельзя исправить, например задекларировать переменную window, файл index.js для свагера не трогал, т.к это не код

--- public/main.js
+++ public/main.js
@@ -1,17 +1,14 @@
 (function () {
-    'use strict';
+  'use strict';
+  /*function plural(n) {
+    if (12 <= n && n <= 14)
+      return (n + ' раз!');
+    if (n % 10 === 2 || n % 10 === 3 || n % 10 === 4)
+      return (n + ' раза!');
+    return (n + ' раз!');
+  }*/
 
-    function plural(n) {
-        if (12 <= n && n <= 14)
-            return (n + ' раз!');
-        if (n % 10 === 2 || n % 10 === 3 || n % 10 === 4)
-            return (n + ' раза!');
-        return (n + ' раз!');
-    }
-
-	if (typeof window === 'object') {
-	    window.SIGN_IN();
-
-    }
-
+  if (typeof window === 'object') {
+    window.SIGN_IN();
+  }
 })();
