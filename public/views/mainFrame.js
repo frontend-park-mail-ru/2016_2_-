c8cfa4d3795b073c0649f2d6f9953d70a29297b8
burmistrovm <burmistrowm@mail.ru>
2016-11-13 22:16:09 +0300

    Добавил игровое окно

---
+++ public/views/mainFrame.js
@@ -0,0 +1,89 @@
+(function () {
+  'use strict';
+  const View = window.View;
+  const Form = window.Form;
+
+  class gameFrame extends View {
+    constructor(options = {}) {
+      super(options);
+      this._el = document.querySelector('.js-game');
+      this.hide();
+    }
+
+    _init() {
+      var socket = new WebSocket("ws://localhost:8080/game");
+      socket.onopen = function() {
+        alert("Соединение установлено.");
+      };
+
+      socket.onclose = function(event) {
+        if (event.wasClean) {
+          alert('Соединение закрыто чисто');
+        } else {
+          alert('Обрыв соединения'); // например, "убит" процесс сервера
+        }
+        alert('Код: ' + event.code + ' причина: ' + event.reason);
+      };
+
+      socket.onmessage = function(event) {
+        alert("Получены данные " + event.data);
+      };
+
+      socket.onerror = function(error) {
+        alert("Ошибка " + error.message);
+      };      
+      /*let WebSocketServer = require('websocket').server;
+      let WebSocketClient = require('websocket').client;
+      let WebSocketFrame  = require('websocket').frame;
+      let WebSocketRouter = require('websocket').router;
+      var client = new WebSocketClient();
+ 
+      client.on('connectFailed', function(error) {
+        console.log('Connect Error: ' + error.toString());
+      });
+ 
+      client.on('connect', function(connection) {
+        console.log('WebSocket Client Connected');
+        connection.on('error', function(error) {
+          console.log("Connection Error: " + error.toString());
+        });
+        connection.on('close', function() {
+          console.log('echo-protocol Connection Closed');
+        });
+        connection.on('message', function(message) {
+            if (message.type === 'utf8') {
+              console.log("Received: '" + message.utf8Data + "'");
+            }
+        });
+    
+        function sendNumber() {
+          if (connection.connected) {
+              var number = Math.round(Math.random() * 0xFFFFFF);
+              connection.sendUTF(number.toString());
+              setTimeout(sendNumber, 1000);
+          }
+        }
+        sendNumber();
+      });
+ 
+      client.connect('ws://localhost:8080/', 'echo-protocol');*/
+
+  }
+
+    _initCanvas () {
+      this.canvas = this._el.querySelector('.js-canvas');
+      this.canvas.width = this._el.clientWidth + '';
+      this.canvas.height = this._el.clientHeight + '';
+    }
+
+    resume () {
+      super.resume();
+      this._initCanvas();
+      this._init();
+    }
+  }
+
+  // export
+  window.gameFrame = gameFrame;
+
+})();
